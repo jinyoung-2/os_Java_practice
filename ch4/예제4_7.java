122e8195fbae58e8dde305084ecd955e14b1c54c
brenda <brenda524@naver.com>
2021-09-24 19:29:03 +0900

    commit

--- ch4/예제4_7.java
+++ ch4/예제4_7.java
@@ -1,11 +1,23 @@
 package oosProject.ch4;
 
+import java.util.Scanner;
+
 public class 예제4_7 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         Book[] book = new Book[2];
         for(int i=0;i< book.length;i++){
-            book[i]=new Book();
+            System.out.print("제목>>");
+            String title = in.nextLine();
+            System.out.print("저자>>");
+            String author = in.nextLine();
+            book[i]=new Book(title,author);
         }
+        for(int i=0;i< book.length;i++){
+            System.out.print("("+book[i].title+","+book[i].author+")");
+        }
+        in.close();
     }
 }
 
